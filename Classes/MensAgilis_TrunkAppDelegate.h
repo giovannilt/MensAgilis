e7aaa6e1c05b91241bd2056d2fd7c63289c61cc7
Giovanni La Torre <giovannilt@gmail.com>
2011-01-29 20:14:59 +0100

    The Project

---
+++ Classes/MensAgilis_TrunkAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MensAgilis_TrunkAppDelegate.h
+//  MensAgilis-Trunk
+//
+//  Created by Giovanni La Torre on 1/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MensAgilis_TrunkAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
